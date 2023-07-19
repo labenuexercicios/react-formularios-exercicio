eba14d9aca900ddeec6be2c15a8627bb37f7c614
Índio Medeiros <71137294+indiomedeiros@users.noreply.github.com>
2023-07-19 17:44:29 -0300

    [style][refactor] Refatora estilização no formulário

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { MainContainer, Form, Input } from './styles'
+import { MainContainer, Form, Input, Button } from './styles'
 
 function MainPage() {
  
@@ -11,23 +11,27 @@ function MainPage() {
         <label htmlFor="nome">Nome do Curso:</label>
         <Input 
           id="nome"
+          placeholder="Digite o nome do curso"
         />
 
         <label htmlFor="modulos">Número de Módulos: </label>
         <Input 
           id="modulos"
+          placeholder="Digite o número de módulos"
         />
 
         <label htmlFor="tecnologias">Tecnologias abordadas: </label>
         <Input 
           id="tecnologias"
+          placeholder="Digite as tecnologias abordadas"
         />
         
         <label htmlFor="responsavel">Responsável: </label>
         <Input 
           id="responsavel"
+          placeholder="Digite o nome do responsável"
         />
-      <button type="submit">Criar curso</button>
+      <Button type="submit">Criar curso</Button>
       </Form>
     </MainContainer>
   )
